b3f81ecf5f46df51ed98cc46a4cb0b0676c5c8b4
Enigmify <69073505+TERIHAX@users.noreply.github.com>
2023-09-23 12:23:41 +0100

    Update UILib.lua

--- Other/UILib.lua
+++ Other/UILib.lua
@@ -1,1 +1,3 @@
-loadstring(game:HttpGet("https://github.com/AirexLabs/PikaUI/raw/main/PikachuUI.lua", true))()
+local library = loadstring(game:HttpGet("https://github.com/AirexLabs/PikaUI/raw/main/PikachuUI.lua", true))()
+
+return library
